a67d3a86c208ea48081cb3fd350e42b9fdb16ead
Oung Seik <mhemaungthuwin@gmail.com>
2026-07-26 23:45:32 +0630

    feat(tmux): customize nova colors

--- modules/homeModules/tmux-module.nix
+++ modules/homeModules/tmux-module.nix
@@ -16,9 +16,20 @@
             set -g @nova-nerdfonts false
             set -g @nova-pane "#{?window_active,,}#I:#W"
 
+            set -g @nova-pane-active-border-style "colour8"
+            set -g @nova-pane-border-style "colour0"
+            set -g @nova-status-style-bg "default"
+            set -g @nova-status-style-fg "colour3"
+            set -g @nova-status-style-active-bg "default"
+            set -g @nova-status-style-active-fg "colour6"
+            set -g @nova-status-style-double-bg "default"
+
             set -g @nova-segment-os " ::"
+            set -g @nova-segment-os-colors "default colour4"
             set -g @nova-segment-computer " :#h"
+            set -g @nova-segment-computer-colors "default colour5"
             set -g @nova-segment-session " :#S"
+            set -g @nova-segment-session-colors "default colour4"
 
             set -g @nova-rows 0
             set -g @nova-segments-0-left "os"
