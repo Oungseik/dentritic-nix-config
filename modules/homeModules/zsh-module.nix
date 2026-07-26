ff7b043779b4aaf139323c8dfe1ade0671aa841c
Oung Seik <mhemaungthuwin@gmail.com>
2026-07-26 23:12:57 +0630

    Configure selected Zsh behavior

--- modules/homeModules/zsh-module.nix
+++ modules/homeModules/zsh-module.nix
@@ -6,6 +6,33 @@
       enableCompletion = true;
       syntaxHighlighting.enable = true;
 
+      history = {
+        append = true;
+        share = true;
+        ignoreSpace = true;
+        ignoreAllDups = true;
+        saveNoDups = true;
+        findNoDups = true;
+      };
+
+      initContent = ''
+        bindkey -e
+        bindkey '^P' history-beginning-search-backward
+        bindkey '^N' history-beginning-search-forward
+        bindkey '^[[1;5C' forward-word
+        bindkey '^[[1;5D' backward-word
+
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+
+        autoload -U select-word-style
+        select-word-style bash
+
+        autoload -Uz edit-command-line
+        zle -N edit-command-line
+        bindkey '^X^E' edit-command-line
+      '';
+
       shellAliases = {
         ls = "eza";
         la = "eza -la";
