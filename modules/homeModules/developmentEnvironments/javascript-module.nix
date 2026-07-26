d8ceeca522641ed4af8e83a16f848ab8d307511f
Oung Seik <mhemaungthuwin@gmail.com>
2026-07-26 17:51:22 +0630

    feat(home): configure package manager paths

--- modules/homeModules/developmentEnvironments/javascript-module.nix
+++ modules/homeModules/developmentEnvironments/javascript-module.nix
@@ -1,16 +1,35 @@
 { ... }: {
-  flake.homeModules.javascriptDevelopmentEnvironment = { pkgs, ... }: {
-    home.file.".local/share/debuggers/dapDebugServer.js".source =
-      "${pkgs.vscode-js-debug}/lib/node_modules/js-debug/dist/src/dapDebugServer.js";
+  flake.homeModules.javascriptDevelopmentEnvironment =
+    { config, pkgs, ... }:
+    let
+      home = config.home.homeDirectory;
+    in
+    {
+      home.sessionVariables = {
+        BUN_INSTALL = "${home}/.bun";
+        NPM_CONFIG_PREFIX = "${home}/.npm-global";
+        PNPM_HOME = "${home}/.local/share/pnpm";
+        YARN_GLOBAL_FOLDER = "${home}/.local/share/yarn/global";
+      };
+      home.sessionPath = [
+        "${home}/.bun/bin"
+        "${home}/.npm-global/bin"
+        "${home}/.local/share/pnpm"
+        "${home}/.yarn/bin"
+      ];
 
-    home.packages = with pkgs; [
-      nodejs
-      bun
-      turbo
-      prettier
-      svelte-language-server
-      vscode-langservers-extracted
-      vscode-js-debug
-    ];
-  };
+      home.file.".local/share/debuggers/dapDebugServer.js".source =
+        "${pkgs.vscode-js-debug}/lib/node_modules/js-debug/dist/src/dapDebugServer.js";
+
+      home.packages = with pkgs; [
+        nodejs
+        bun
+        pnpm
+        turbo
+        prettier
+        svelte-language-server
+        vscode-langservers-extracted
+        vscode-js-debug
+      ];
+    };
 }
