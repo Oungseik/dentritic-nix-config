1aaadb151d032c038aad646fe027e957139a78bd
Oung Seik <mhemaungthuwin@gmail.com>
2026-09-05 13:13:01 +0630

    chore: update flake

--- modules/nixosModules/gaming-module.nix
+++ modules/nixosModules/gaming-module.nix
@@ -5,6 +5,12 @@
   flake.nixosModules.gaming =
     { pkgs, ... }:
     {
+      nix.settings = {
+        substituters = [ "https://attic.xuyh0120.win/lantian" ];
+        trusted-public-keys = [
+          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        ];
+      };
 
       hardware.graphics = {
         enable = true;
