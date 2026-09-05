133367033af4020bb154d9126f1b29b49cefe07c
Oung Seik <mhemaungthuwin@gmail.com>
2026-09-05 11:53:26 +0630

    chore: update bun

--- modules/homeModules/developmentEnvironments/javascript-module.nix
+++ modules/homeModules/developmentEnvironments/javascript-module.nix
@@ -24,10 +24,10 @@
       home.packages = with pkgs; [
         nodejs
         (bun.overrideAttrs (_: rec {
-          version = "1.4.0";
+          version = "1.4.1";
           src = fetchurl {
             url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-            hash = "sha256-LQP7X7g6yLVnrKCigbLOGhoZ1Ij1bClo2Iw/Jekv5FI=";
+            hash = "sha256-dMHDvufNmYUAyPlpzYlyNVrGoHIH6Uo57s4ZmbVv+r8=";
           };
         }))
         deno
